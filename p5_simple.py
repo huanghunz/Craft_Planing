1984ab9ae40eaad27274df81623f4c5fec0118ae
cdilday <cdilday@ucsc.edu>
2015-05-06 01:04:04 -0700

    got it working with heuristic and some stuff with working backwards

--- p5_simple.py
+++ p5_simple.py
@@ -1,7 +1,5 @@
-
-
-from collections import namedtuple
-import time
+from collections import namedtuple, Counter
+import time, copy
 TIME_LIMIT = 30
 try:
     import Queue as Q  # ver. < 3.0
@@ -36,7 +34,10 @@ except ImportError:
 
 
 all_recipes = []
+recipe_dict = Counter()
 Crafting = None
+
+maxes = {}
 
 
 #Where are make_checker and make_effector defined? You have to do that as well:
@@ -161,7 +162,8 @@ def search(graph, initial, is_goal, limit, heuristic):
 		adj = graph(currState)
 
 		for state in adj:
-			if state[0] not in prev: # or node[0] < heuristic(node) : #heuristic
+			if state[0] not in prev : #heuristic
+				state = ((state[0][0] + heuristic(state[0]), state[0][1]), state[1] )
 				priorityQueue.put(state[0])
 				prev[state[0]] = currState
 				step[state[0]] = state[1]
@@ -172,6 +174,7 @@ def search(graph, initial, is_goal, limit, heuristic):
 	
 	if t_now > t_deadline:
 		print "time out"
+		print currState
 		total_cost = -1
 
 	else: 
@@ -189,6 +192,64 @@ def search(graph, initial, is_goal, limit, heuristic):
 	return total_cost, plan
 
 
+def search_backward(graph, initial, is_goal, limit, heuristic):
+	total_cost = 0
+	plan = []
+	prev = {initial: None}
+	step = {initial: None}
+
+	priorityQueue = Q.PriorityQueue()
+
+	priorityQueue.put(initial)
+
+	t_now = time.time()
+	t_deadline = t_now + TIME_LIMIT
+
+
+	run = 0
+	while not priorityQueue.empty() and t_now < t_deadline:
+		currState = priorityQueue.get()
+
+		if is_goal(currState):
+			break
+
+		#get list of possible states
+		adj = graph(currState)
+
+		#print " == "
+
+		
+
+		for state in adj:
+			if state[0] not in prev: # or node[0] < heuristic(node) : #heuristic
+				priorityQueue.put(state[0])
+				prev[state[0]] = currState
+				step[state[0]] = state[1]
+
+				
+		run += 1
+		t_now = time.time()
+
+	
+	if t_now > t_deadline:
+		print "time out"
+		total_cost = -1
+
+	else: 
+		#build path
+		total_cost = currState[0]
+		while currState:
+			#plan.append(currState) # appending the inventory of each state
+			#plan.append((currState, step[currState])) # appending the invertory and each step
+			plan.append(step[currState]) # appending each step 
+			currState = prev[currState]
+
+		plan.remove(step[initial])
+
+		
+	return total_cost, plan
+
+
 def t_graph(state):
 	
 	adj = []
@@ -198,6 +259,77 @@ def t_graph(state):
 			newState = (newState[0] + recipe[3], newState[1])
 			adj.append((newState, recipe[0])) # returning the step of the plan
 	return adj
+
+def b_graph(state):
+	adj = []
+	item_list = list(state[1]) # (name, count)
+	#items = dict(item_list)
+	tick = 0
+	for item in item_list:
+		#print " right in the for loop:"
+		#print item
+		#print
+		rec = recipe_dict[item[0]]
+		for r in rec:
+			new_list = None
+			new_list = copy.copy(item_list)
+
+			pindex = 0
+			new_pro = None
+
+			if r[1]:
+				for pro in r[1].items():
+					for producable in item_list:
+						if pro[0] == producable[0]:
+							index = item_list.index(producable)
+							if producable[1] - pro[1] < 0:
+								new_pro = (producable[0], 0) 
+							else:	
+								new_pro = (producable[0], producable[1] - pro[1]) 
+							break
+			
+
+			if r[2]:
+				for req in r[2].items():
+					in_list = False
+					for thing in item_list:
+						if req[0] == thing[0]:
+							in_list = True
+							break
+
+					if not in_list:
+						new_list.append(req)
+
+			if r[3]:
+				for con in r[3].items():
+					found = False
+					index = 0
+					new_con = None
+					for consumable in item_list:
+						if con[0] == consumable[0]:
+							index = new_list.index(consumable)
+							new_con = (consumable[0], consumable[1] + con[1]) 
+							found = True
+							break
+
+					if found:
+						new_list[index] = new_con
+					else:
+						new_list.append(con)
+
+			if new_pro:
+				if new_pro[1] == 0:
+					new_list.remove(new_list[pindex])
+				else:
+					new_list[pindex] = new_pro
+
+			adj.append( (         (            state[0] + r[4],               tuple(new_list)               )              , r[0]               )   )
+		
+
+	return adj
+
+
+
 
 def t_is_goal(state):
 	item_list = list(state[1]) # (name, count)
@@ -210,7 +342,30 @@ def t_is_goal(state):
 			return False
 	return True
 
+def b_is_goal(state):
+	item_list = list(state[1]) # (name, count)
+	items = dict(item_list)
+
+	if not Crafting['Initial'] and items:
+		return False
+
+	for item in Crafting['Initial'] :
+		if item not in items or Crafting['Initial'][item] < items[item]:
+			return False
+
+	return True
+
+
 def t_heuristic(state):
+	item_list = list(state[1]) # (name, count)
+	items = dict(item_list)
+
+	for item in items:
+		#print item
+		if item in maxes and items[item] > maxes[item]:
+			return 50
+
+
 	return 0
 
 
@@ -226,13 +381,45 @@ if __name__ ==  '__main__':
 		effector = make_effector(rule)
 		recipe = Recipe(action, checker, effector, rule['Time'])
 		all_recipes.append(recipe)
+
+		pro = rule.get('Produces', [])
+		con = rule.get('Consumes', [])
+		req = rule.get('Requires', [])
+		cost = rule['Time']
+
+		for c in con:
+			if c not in maxes or maxes[c] < con[c]:
+				maxes[c] = con[c]
+
+		for r in req:
+			if r not in maxes or maxes[r] < req[r]:
+				maxes[r] = req[r]
+
+		key = pro.keys()[0]
+		recipe_dict.setdefault(key, [])
+
+		recipe_dict[key].append((action, pro, req, con, cost))
+
+	#print recipe_dict['wood']
 	
 	#Crafting['Initial'] = {'bench': 1, 'plank': 3, 'stick': 2}
-	Crafting['Goal'] =  {'furnace': 1}
+	Crafting['Goal'] =  { 'rail':1}
+
+	for g in Crafting['Goal']:
+
+		if g in maxes and Crafting['Goal'][g] > maxes[g]:
+			maxes[g] = Crafting['Goal'][g]
+
+	for m in maxes:
+		print m, ' ', maxes[m]
 
 	initState = (0, tuple(Crafting['Initial'].items()))
 
-	total_cost, plan = search (t_graph, initState, t_is_goal, float("inf"), t_heuristic)
+	startTime = time.time()
+
+
+	total_cost, plan = search(t_graph, initState, t_is_goal, float("inf"), t_heuristic)
+
 
 	print "Done"
 	print "Goal is: ", Crafting['Goal']
@@ -241,8 +428,5 @@ if __name__ ==  '__main__':
 	for step in plan:
 		print step
 	print "Total steps: ",len(plan) 
-
-
-
-
+	print "Time taken: ", time.time() - startTime
 
